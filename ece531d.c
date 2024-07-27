b06636c3ff96c066915b01071e25e834656a5239
Sean Bruno <sbruno@ignoranthack.me>
2024-07-27 21:23:12 +0000

    While the thermocouple file is valid, reopen and reread its data value

--- ece531d.c
+++ ece531d.c
@@ -14,7 +14,7 @@
 
 
 const char progname[] = "ece531d";
-char therm_file[] = "/var/log/temperature";
+char therm_file[] = "/var/log/temp";
 
 //void openlog(const char *ident, int option, int facility);
 //void syslog(int priority, const char *format, ...);
@@ -86,22 +86,26 @@ int main(int argc, char argv[])
   // Check for /var/log/temperatur, error and exit if failure with log message
   // Else we will read from it forever.
   // while forever, do sleep 1 and log time
-  while ( 1 ) {
+  do {
     struct timeval current_tv;
     time_t now_seconds;
     struct tm *now_tm;
     char time_buf[64];
+    ssize_t num_chars_read;
+    char thermo_buf[128];
 
+    num_chars_read = fread(thermo_buf, sizeof(char), 64, temperature);
     gettimeofday(&current_tv, NULL);
     // Get human readable localtime()
     now_seconds = current_tv.tv_sec;
     now_tm = localtime(&now_seconds);
 
     strftime(time_buf, sizeof(time_buf), "%Y-%m-%d %H:%M:%S", now_tm);
-    syslog(LOG_INFO, "%s: time is now %ld %s\n", progname, current_tv.tv_sec, time_buf);
+    syslog(LOG_INFO, "%s: time is now %ld %s. temperature is %s\n", progname, current_tv.tv_sec, time_buf, thermo_buf);
+    fclose(temperature);
     sleep(1);
-  } 
+  } while ((temperature = fopen(therm_file,"r")) != NULL) ;
 
-  syslog(LOG_ERR, "%s: SHOULD NOT BE HERE\n", progname);
+  syslog(LOG_ERR, "%s: SHOULD NOT BE HERE error opening thermocouple file: %s\n", progname, strerror(errno));
   return 0;
 }
